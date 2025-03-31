00506d0c322f2daef60cf88c3f9d023322cf4928
T4D4 .icp <t4d4.icp@gmail.com>
2025-03-31 11:38:09 +0000

    dfx.nixを更新し、fetchzipからfetcFromGitHubに変更して、GitHubからのソース取得を改善

--- pkgs/dfx.nix
+++ pkgs/dfx.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchzip }:
+{ stdenv, fetcFromGitHub }:
 
 let
   version = "0.25.0"; # バージョンを変数として定義
@@ -7,14 +7,10 @@ stdenv.mkDerivation {
   pname = "dfx";
   inherit  version; # バージョンを継承
 
-  src = fetchzip {
-    url = "https://github.com/dfinity/sdk/releases/download/${version}/dfx-${version}-x86_64-linux.tar.gz";
-    sha256 = "OWqmvgMd2dT8rSlrrLtfmNm1/sjYa1h6+AbpX7uWdnk="; # SHA-256 ハッシュ
+  src = fetcFromGitHub {
+    owner = dfinity;
+    repo = "sdk";
+    rev = "v${version}";
+    sha256 = "0hjz2xjahj2byzbbdf6x47qafdffhcgv2ikrwbh5mbcxp4iy41ww"; # SHA-256 ハッシュ
   };
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin
-    cp -r * $out/bin
-    runHook postInstall
-  '';
 }
