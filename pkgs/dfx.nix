fda297fe7f75634871b8b93b0f96920a42b3adff
T4D4 .icp <t4d4.icp@gmail.com>
2025-03-31 11:59:14 +0000

    dfx.nixをバージョン0.25.1に更新し、fetchurlを使用してソースを取得するように変更

--- pkgs/dfx.nix
+++ pkgs/dfx.nix
@@ -1,16 +1,25 @@
-{ stdenv, fetchFromGitHub }:
+{ stdenv, fetchurl, autoPatchelfHook }:
 
 let
-  version = "0.25.0"; # バージョンを変数として定義
+  version = "0.25.1";  # 最新の安定版に更新
+  system = if stdenv.isDarwin then "x86_64-darwin" else "x86_64-linux";
 in
 stdenv.mkDerivation {
   pname = "dfx";
-  inherit  version; # バージョンを継承
+  inherit version;
 
-  src = fetchFromGitHub {
-    owner = "dfinity";
-    repo = "sdk";
-    rev = "v${version}";
-    sha256 = "0hjz2xjahj2byzbbdf6x47qafdffhcgv2ikrwbh5mbcxp4iy41ww"; # SHA-256 ハッシュ
+  src = fetchurl {
+    url = "https://github.com/dfinity/sdk/releases/download/${version}/dfx-${version}-${system}.tar.gz";
+    sha256 = ""; # バイナリの正しいSHA256ハッシュを追加する必要があります
   };
+
+  nativeBuildInputs = stdenv.lib.optional stdenv.isLinux autoPatchelfHook;
+
+  sourceRoot = ".";
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp dfx $out/bin/
+    chmod +x $out/bin/dfx
+  '';
 }
