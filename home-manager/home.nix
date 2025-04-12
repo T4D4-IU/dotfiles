9e744ef769b1be718bb1ea1a6a8d3b6a3f7d7972
T4D4 .icp <t4d4.icp@gmail.com>
2025-04-12 14:58:28 +0000

    zsh設定を別ファイルに切り出し

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -20,40 +20,12 @@
     ./direnv.nix
     ./development.nix
     ./zed.nix
+    ./zsh.nix
   ];
 
   programs.zoxide = {
     enable = true;
     enableZshIntegration = true;
-  };
-
-  programs.zsh = {
-    enable = true;
-    autocd = true;
-    enableCompletion = true;
-    autosuggestion = {
-      enable = true;
-    };
-    syntaxHighlighting.enable = true;
-    shellAliases = {
-      # zshで呼ぶコマンドのエイリアスを設定
-      cd = "z";
-      ls = "eza --icons always --classify always";
-      la = "eza --icons always --classify always --all";
-      ll = "eza --icons always --git --long";
-      tree = "eza --icons always --classify always --tree";
-      cat = "bat";
-      grep = "rg";
-      du = "dust";
-      df = "duf";
-      ps = "procs";
-      top = "btm";
-      diff = "delta";
-      find = "fd";
-      vim = "nix run 'github:T4D4-IU/mynixvim'";
-      lg = "lazygit";
-      dfx = "nix run 'github:T4D4-IU/dotfiles#dfx'";
-    };
   };
 
   # The home.packages option allows you to install Nix packages into your
---
+++ home-manager/zsh.nix
@@ -0,0 +1,30 @@
+{
+    programs.zsh = {
+        enable = true;
+    autocd = true;
+    enableCompletion = true;
+    autosuggestion = {
+      enable = true;
+    };
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      # zshで呼ぶコマンドのエイリアスを設定
+      cd = "z";
+      ls = "eza --icons always --classify always";
+      la = "eza --icons always --classify always --all";
+      ll = "eza --icons always --git --long";
+      tree = "eza --icons always --classify always --tree";
+      cat = "bat";
+      grep = "rg";
+      du = "dust";
+      df = "duf";
+      ps = "procs";
+      top = "btm";
+      diff = "delta";
+      find = "fd";
+      vim = "nix run 'github:T4D4-IU/mynixvim'";
+      lg = "lazygit";
+      dfx = "nix run 'github:T4D4-IU/dotfiles#dfx'";
+    };
+    };
+}
