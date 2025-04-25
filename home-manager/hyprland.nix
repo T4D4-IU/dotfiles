19ef6a26a8c2a28c9aa4d16c243e1a1bb6e262de
T4D4 .icp <t4d4.icp@gmail.com>
2025-04-25 14:26:24 +0000

    hyprland.nixの設定を更新し、必要なパッケージとキーバインディングを追加

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -1,4 +1,10 @@
 {pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
+    wofi
+    waybar
+    grimblast
+    hyprpaper
+  ];
   programs.kitty.enable = true;
   wayland.windowManager.hyprland = {
     enable = true;
@@ -6,11 +12,16 @@
     portalPackage = null;
     settings = {
       "$mod" ="SUPER";
+      "$menu" = "wofi --show drun";
       bind = [
         "$mod, G, exec, ghostty"
         "$mod, C, exec, code"
-        "$mod, S, exec, supotify"
+        "$mod, S, exec, spotify"
         "$mod, B, exec, brave"
+        "$mod, Q, killactive"
+        ", Print, exec, grimblast copy area"
+        "$mod, D, exec, $menu"
+        "$mod, M, exit"
       ];
     };
     plugins = [
