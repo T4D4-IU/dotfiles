cb555d6d75824b62a40c9ea72e49ad6c4689fd5c
t4d4 <t4d4.icp@gmail.com>
2025-04-27 23:16:35 +0900

    add emoji txt

--- home-manager/rofi.nix
+++ home-manager/rofi.nix
@@ -4,4 +4,9 @@
         package = pkgs.rofi-wayland;
         theme = ./rofi.rasi; # referd from https://github.com/turtton/dotnix/blob/main/home-manager/wm/hyprland/rofi/default.nix
     };
+    home.packages = with pkgs; [
+        bemoji
+    ];
+    # https://github.com/marty-oehme/bemoji?tab=readme-ov-file#adding-your-own-emoji
+    xdg.dataFile."bemoji/shortcodes.txt".source = ./bemoji.txt;
 }
