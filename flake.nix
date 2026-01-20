c799cdbf88114d28db2abaadeecac5547fea4447
t4d4 <t4d4.icp@gmail.com>
2026-01-20 19:21:25 +0900

    fix: Apply final linter recommendations
    - hardware-configuration.nix: Remove unused config and lib parameters (deadnix)
    - flake.nix: Use inherit (hosts.*) syntax for cleaner code (statix)

--- flake.nix
+++ flake.nix
@@ -93,18 +93,14 @@
     homeConfigurations = {
       "t4d4@nixos" = helpers.mkHomeConfiguration {
         inherit inputs;
-        system = hosts.nixos.system;
-        username = hosts.nixos.username;
-        homeDirectory = hosts.nixos.homeDirectory;
+        inherit (hosts.nixos) system username homeDirectory;
         modules = hosts.nixos.homeModules;
       };
 
       # WSL/Ubuntu environment (CLI-only)
       "t4d4@wsl" = helpers.mkHomeConfiguration {
         inherit inputs;
-        system = hosts.wsl.system;
-        username = hosts.wsl.username;
-        homeDirectory = hosts.wsl.homeDirectory;
+        inherit (hosts.wsl) system username homeDirectory;
         modules = hosts.wsl.homeModules;
       };
     };
