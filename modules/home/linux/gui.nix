ec2909e42aefb6889bd363de25b4a263a0d91b5c
t4d4 <t4d4.icp@gmail.com>
2026-01-20 22:51:47 +0900

    nix flake check が通らないので重いパッケージをコメントアウトした

--- modules/home/linux/gui.nix
+++ modules/home/linux/gui.nix
@@ -7,19 +7,19 @@
     anki
     zulip
     slack
-    telegram-desktop
+    # telegram-desktop
     spotify
     syncthing
     rquickshare # rust implementation of quickshare
-    wireshark # Powerful network protocol analyzer
+    # wireshark # Powerful network protocol analyzer
     networkmanagerapplet # network manager gui
     blueberry # Bluetooth configuration tool
 
     # Wayland/Linux GUI utilities
-    grimblast # Helper for screenshots within Hyprland, based on grimshot
-    hyprpaper # Blazing fast wayland wallpaper utility
-    cliphist # Wayland clipboard History
-    wl-clipboard # Wayland clipboard manager
-    brightnessctl # Screen brightness control
+    # grimblast # Helper for screenshots within Hyprland, based on grimshot
+    # hyprpaper # Blazing fast wayland wallpaper utility
+    # cliphist # Wayland clipboard History
+    # wl-clipboard # Wayland clipboard manager
+    # brightnessctl # Screen brightness control
   ];
 }
