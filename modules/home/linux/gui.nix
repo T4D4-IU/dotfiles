bab697cd4325d6553956ecea59d8d925a341839c
t4d4 <t4d4.icp@gmail.com>
2026-01-21 00:49:04 +0900

    まだ重いので他のGUIアプリもコメントアウト

--- modules/home/linux/gui.nix
+++ modules/home/linux/gui.nix
@@ -1,19 +1,19 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    # GUI Applications
-    brave
-    discord
-    obsidian
-    anki
-    zulip
-    slack
+    # GUI Applications "nix flake checkを実行しようとしたら容量がデカくて死んだので重そうなGUIアプリを全部コメントアウトして無効化している"
+    # brave
+    # discord
+    # obsidian
+    # anki
+    # zulip
+    # slack
     # telegram-desktop
-    spotify
-    syncthing
-    rquickshare # rust implementation of quickshare
+    # spotify
+    # syncthing
+    # rquickshare # rust implementation of quickshare
     # wireshark # Powerful network protocol analyzer
-    networkmanagerapplet # network manager gui
-    blueberry # Bluetooth configuration tool
+    # networkmanagerapplet # network manager gui
+    # blueberry # Bluetooth configuration tool
 
     # Wayland/Linux GUI utilities
     # grimblast # Helper for screenshots within Hyprland, based on grimshot
