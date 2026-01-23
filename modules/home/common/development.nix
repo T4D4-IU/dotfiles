54169cfb9a943f345dcf58fce4af602e5578de37
T4D4 <98306330+T4D4-IU@users.noreply.github.com>
2026-01-23 13:19:08 +0900

    Refactor/organize modules (#12)
    * git.nixの追加とghとzoxideとzedを移動
    
    * rename

--- modules/home/common/development.nix
+++ modules/home/common/development.nix
@@ -1,25 +1,10 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     # warp-terminal
-    gcc
-    nodejs-slim # npmのないNode.js単体
-    nodePackages.pnpm
     # nodePackages.wrangler # Cloudflare Workers CLI
-    deno
-    bun
     # postman
     # mise
-    foundry
     # wasmer # ユニバーサルWebAssemblyランタイム - ビルドエラーのため一時的にコメントアウト
-    (rust-bin.stable.latest.default.override {
-      targets = [
-        "wasm32-unknown-unknown"
-        # "wasm32-wasi" # 最新のStable版ではサポートされていない？
-      ];
-    })
-    cargo-shuttle
-    cargo-llvm-cov # Rustのカバレッジ計測ツール
-    cargo-binutils # cargo-llvm-covに使う
     # vscode
     # jetbrains-toolbox
     # zenn-cli # textlintとpnpm lock.yamlが競合するため一時的にコメントアウト
