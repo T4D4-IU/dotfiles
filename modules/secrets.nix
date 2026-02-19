d8b7e154517eb30e893cba0d974fc437ac12fb37
t4d4 <t4d4.icp@gmail.com>
2026-02-19 20:06:40 +0900

    feat: setup sops-nix with private secrets repo

---
+++ modules/secrets.nix
@@ -0,0 +1,17 @@
+
+{ inputs, config, ... }:
+{
+  imports = [
+    inputs.sops-nix.nixosModules.sops
+  ];
+
+  sops = {
+    defaultSopsFile = "${inputs.my-secrets}/secrets.yaml";
+    defaultSopsFormat = "yaml";
+
+    #age.keyFile = "/home/t4d4/.config/sops/age/keys.txt";
+    age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+
+    secrets.test_secret = {};
+  };
+}
